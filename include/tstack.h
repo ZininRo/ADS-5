6786911dc45f4215613a91658c3d053bb8b08ec0
ZininRo <romanzinin2005@gmail.com>
2024-04-07 21:58:12 +0300

    ADS-5 Roman Zinin

--- include/tstack.h
+++ include/tstack.h
@@ -1,36 +1,37 @@
+// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
 #include <stdexcept>
 
 template<typename T, int kSize>
 class TStack {
-    private:
-        T data[kSize];
-        int top = 0;
+ private:
+     T data[kSize];
+     int top = 0;
 
-    public:
-        void push(const T &item) {
-            if (top >= kSize)
-                throw std::overflow_error("Full!");
-            data[top++] = item;
-        }
+ public:
+     void push(const T &item) {
+         if (top >= kSize)
+             throw std::overflow_error("Full!");
+         data[top++] = item;
+     }
 
-        T pop() {
-            if (top <= 0)
-                throw std::underflow_error("Empty!");
-            return data[--top];
-        }
+     T pop() {
+         if (top <= 0)
+             throw std::underflow_error("Empty!");
+          return data[--top];
+     }
 
-        bool isEmpty() const {
-            return top == 0;
-        }
+     bool isEmpty() const {
+         return top == 0;
+     }
 
-        const T& get() const {
-            if (isEmpty())
-                throw std::underflow_error("Empty!");
-            return data[top - 1];
-        }
+     const T& get() const {
+         if (isEmpty())
+             throw std::underflow_error("Empty!");
+         return data[top - 1];
+     }
 };
 
 #endif  // INCLUDE_TSTACK_H_
+
